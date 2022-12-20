86a841f55e65655750d458c94f4faebeea87ba04
heky <heji12356@gmai.com>
2022-12-20 16:23:54 +0800

    4

--- 单词.py
+++ 单词.py
@@ -1,12 +1,4 @@
 import random
-# 自动录入单词的函数
-def add_word():
-    english_word = input('Please enter the English word: ')
-    chinese_meaning = input('Please enter the Chinese meaning: ')
-
-    # 将单词写入文件
-    with open('words.txt', 'a', encoding='utf-8') as f:
-        f.write(f'{english_word},{chinese_meaning}\n')
 
 # 回答错了之后输出正确的答案
 def show_correct_answer(english_word, chinese_meaning):
@@ -60,47 +52,9 @@ def update_words(words, passed_words, failed_words):
     for word in passed_words:
         words.remove(word)
     return words
-# 选择提
-def changes():
-    # 读取单词列表
-    with open('words.txt', 'r', encoding='utf-8') as f:
-        words = []
-        for line in f:
-            words.append(line.strip().split(','))
-
-    # 打乱单词顺序
-    random.shuffle(words)
-
-    # 遍历单词列表
-    for english_word, chinese_meaning in words:
-        # 提示用户输入中文意思
-        print(f'What is the meaning of "{english_word}"?:')
-
-        # 提供选项
-        options = [chinese_meaning]
-        while len(options) < 4:
-            word = random.choice(words)
-            if word[1] not in options:
-                options.append(word[1])
-
-        # 打乱选项顺序
-        random.shuffle(options)
-
-        # 遍历选项
-        for i, option in enumerate(options):
-            print(f'{i + 1}. {option}')
-
-        # 读取用户输入
-        user_input = input('Enter your answer: ')
-
-        # 判断用户输入是否正确
-        if user_input == chinese_meaning:
-            print('Correct!')
-        else:
-            print('Incorrect. The correct answer is:', chinese_meaning)
 
 # 从文件中读取单词列表
-with open('words.txt', 'r') as f:
+with open('选择性必修二U1.txt', 'r') as f:
     words = []
     for line in f:
         # 将单词和对应的中文意思放到一个列表中
